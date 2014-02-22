b275b2c06a90909993417c11b40f7da4efab3fad
Timothy Hahn <timyhahn@gmail.com>
2014-02-22 14:21:01 -0500

    ALSO NO DEBUG

--- views.py
+++ views.py
@@ -4,7 +4,6 @@ from models import Route, Base
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/minutes_late')
 def minutes_late():
